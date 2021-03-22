0eadde15a381c387684be04f9990b79802489ae1
paulphoku <paulphoku@gmail.com>
2021-03-22 19:54:44 +0200

    prod mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebaseConfig: {
+    //https://firebase.google.com/docs/web/setup
+    apiKey: "AIzaSyBOQAUC-7FuXIzCwxlsAzGIMNaPVtWD2ms",
+    authDomain: "lebo-m-courier.firebaseapp.com",
+    projectId: "lebo-m-courier",
+    storageBucket: "lebo-m-courier.appspot.com",
+    messagingSenderId: "907749237178",
+    appId: "1:907749237178:web:efe58e6693100562653d83",
+    measurementId: "G-X1PETXZSFE"
+  },
+  tinymce: {
+    //https://www.tiny.cloud/blog/how-to-get-tinymce-cloud-up-in-less-than-5-minutes/
+    apiKey: "3pk5w7ysktp2k6kbdo0girxw2h42l4eeeyc3wew857xn9s50"
+  }};
